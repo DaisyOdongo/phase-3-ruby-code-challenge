18f7bb7aa59ddcaa57ab55b55ad8e6b26a405531
+ <+>
2022-10-30 23:14:24 +0300

    completed

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,6 +4,13 @@ class Author
 
   def initialize(name)
     @name = name
+    @articles = []
+    @magazines = []
+    if name.is_a?(String)
+      @name = name
+    else
+      puts "Expected name.class to be a String"
+    end
   
   end
 
