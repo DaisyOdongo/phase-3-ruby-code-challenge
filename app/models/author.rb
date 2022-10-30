5a3ffe67e5050c6b692f1d61e496a147347bc734
+ <+>
2022-10-30 23:37:36 +0300

    completed

--- app/models/author.rb
+++ app/models/author.rb
@@ -11,6 +11,24 @@ class Author
     else
       puts "Expected name.class to be a String"
     end
+
+    def articles 
+    @articles
+  end
+  def magazines
+    @magazines.uniq
+  end
+  def add_article(magazine, title)
+    if(magazine.is_a?(Magazine) && title.is_a?(String))
+      @articles  << Article.new(self, magazine, title)
+      @magazines << magazine
+    else
+      puts "Expected Magazine.class to be an Object, title.class to be a String"
+    end
+  end
+  def topic_areas
+    @magazines.map {|magazine| magazine.category}.uniq
+  end
   
   end
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,21 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+#Author class test
+chimamanda = Author.new("Chimamanda")
+ngugi = Author.new("Ngugi")
+achebe = Author.new("Achebe")
+
+#Magazine class test
+mens_health = Magazine.new("Mens Health", "Fitness")
+vogue = Magazine.new("Vogue", "Entertainment")
+runner = Magazine.new("Runner", "Sports")
+
+#Articles class test
+achebe.add_article(entertainment, "Entertainment industry in Nigeria making it big")
+chimamanda.add_article(education, "Reading many books help you improve your language")
+ngugi.add_article(politics, "Kenyans still vote along ethic groups")
+
 
 
 
