de101e89af669534f1bf91f804be8f62affa9f9e
+ <+>
2022-10-30 19:19:31 +0300

    first commit

--- solution.rb
+++ solution.rb
@@ -1,1 +1,30 @@
 # Please copy/paste all three classes into this file to submit your solution!
+class Article
+
+
+end
+
+class Author
+  attr_accessor :name
+
+
+  def initialize(name)
+    @name = name
+  
+  end
+
+
+end
+
+class Magazine
+  attr_accessor :name, :category
+
+
+  def initialize(name, category)
+    @name = name
+    @category = category
+
+  end
+
+
+end
